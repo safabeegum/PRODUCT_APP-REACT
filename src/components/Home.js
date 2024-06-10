f5e5ccf9dd3450e8d7b0157dc2c81da6b4109d66
safabeegum <pasafabeegum@gmail.com>
2024-06-11 00:57:25 +0530

    Completed

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="card">
+                <img src="https://img.freepik.com/free-photo/black-friday-elements-assortment_23-2149074075.jpg?t=st=1718047632~exp=1718051232~hmac=8a6f3d043905fbdf59d908180ab9a09aef519591ae9224a8c7dfd03c7c3368ce&w=1060" class="card-img-top" alt="..."></img>
+  
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
